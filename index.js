adddf9c77d214c1ea0d5766813c52c8e978fd80d
Soner Yıldırım <soneryildirimx@gmail.com>
2022-07-30 11:52:57 +0300

    reply pong to ping

---
+++ index.js
@@ -0,0 +1,17 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+dotenv.config();
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.on('messageCreate', message => {
+    if(message.content === 'ping') {
+        message.reply('Pong')
+    }
+});
+
+client.login(process.env.TOKEN);
