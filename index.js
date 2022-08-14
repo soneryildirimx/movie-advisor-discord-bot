c0d5697d3f900b7934424ed604de0cb98d59c008
Soner Yıldırım <soneryildirimx@gmail.com>
2022-08-15 00:41:13 +0300

    daily advise

--- index.js
+++ index.js
@@ -7,19 +7,27 @@ import {
   ActivityType,
 } from "discord.js";
 import { REST } from "@discordjs/rest";
-import { hitMe, getRandomMovieByGenre, getTrending } from "./utils.js";
+import {
+  hitMe,
+  getRandomMovieByGenre,
+  getTrending,
+  getTopRated,
+} from "./utils.js";
 import { commands, genres } from "./enums.js";
 import { CronJob } from "cron";
+import path from "path";
+const __dirname = path.resolve();
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({
+  path: path.resolve(__dirname, `./.${process.env.NODE_ENV}.env`),
+});
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    await rest.put(
-      Routes.applicationCommands(process.env.APP_ID),
-      { body: commands }
-    );
+    await rest.put(Routes.applicationCommands(process.env.APP_ID), {
+      body: commands,
+    });
   } catch (error) {
     console.error(error);
   }
@@ -33,16 +41,23 @@ const client = new Client({
   ],
 });
 
-client.on("ready", () => {
+client.on("ready", async () => {
   console.log(`Logged in as ${client.user.tag}!`);
-
   const setActivityJob = new CronJob("*/120 * * * *", async () => {
     const movie = await getTrending();
     client.user.setActivity(movie, {
       type: ActivityType.Watching,
     });
   });
+  const setDailyAdvise = new CronJob("0 18 * * *", async () => {
+    const guilds = await client.guilds.fetch();
+    guilds.forEach(async (item) => {
+      const guildClass = await client.guilds.cache.get(item.id);
+      await guildClass.systemChannel.send(await getTopRated());
+    });
+  });
   setActivityJob.start();
+  setDailyAdvise.start();
 });
 
 client.on("interactionCreate", async (interaction) => {
@@ -74,5 +89,4 @@ client.on("interactionCreate", async (interaction) => {
     await interaction.editReply(movie);
   }
 });
-
 client.login(process.env.TOKEN);
--- utils.js
+++ utils.js
@@ -1,6 +1,10 @@
 import fetch from "node-fetch";
 import dotenv from "dotenv";
-dotenv.config();
+import path from "path";
+const __dirname = path.resolve();
+dotenv.config({
+  path: path.resolve(__dirname, `./.${process.env.NODE_ENV}.env`),
+});
 const apiKey = process.env.MOVIE_TOKEN;
 const baseUrl = "https://api.themoviedb.org/3";
 const baseQuery = `api_key=${apiKey}&language=en-US`;
@@ -55,6 +59,20 @@ export const getTrending = async () => {
   const response = await fetch(`${baseUrl}/trending/movie/day?${baseQuery}`);
   const { results } = await response.json();
   return results[randomMovie].title;
+};
+export const getTopRated = async () => {
+  while (true) {
+    const randomPage = Math.floor(Math.random() * 500);
+    const randomMovie = Math.floor(Math.random() * 20);
+    const response = await fetch(
+      `${baseUrl}/movie/top_rated?page=${randomPage}&${baseQuery}&append_to_response`
+    );
+    const { results } = await response.json();
+    const movie = await getMovieById(results[randomMovie]?.id);
+    if (movie.imdb_id) {
+      return `https://www.imdb.com/title/${movie.imdb_id}/`;
+    }
+  }
 };
 
 export const sleep = (ms) => {
